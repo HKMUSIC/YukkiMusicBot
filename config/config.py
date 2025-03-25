e72a730d0127215868babf37f8cd9005faa13f74
HKMUSIC <himanshu4hk9@gmail.com>
2025-03-25 12:03:07 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -168,25 +168,25 @@ autoclean = []
 
 
 # Images
-START_IMG_URL = getenv("START_IMG_URL", None)
+START_IMG_URL = getenv("https://imagekit.io/tools/asset-public-link?detail=%7B%22name%22%3A%22b143f84feb666349700b99168db9aee3.jpg%22%2C%22type%22%3A%22image%2Fjpeg%22%2C%22signedurl_expire%22%3A%222028-03-24T06%3A24%3A32.150Z%22%2C%22signedUrl%22%3A%22https%3A%2F%2Fmedia-hosting.imagekit.io%2F%2F14556c5680ca4f51%2Fb143f84feb666349700b99168db9aee3.jpg%3FExpires%3D1837491872%26Key-Pair-Id%3DK2ZIVPTIP2VGHC%26Signature%3Dt1mXeA4JvjLTeDBOQmTWB1ta17MnYxtfSDKJ-3lFRPhL0Y-4iXCXKOmNljamgUXVEbUmG2hkEIQTRr-oVyAi4EB3kAbzjZ2aS~fR6N17jFU-sMPVTCWPt~j8NMVuu6KcMlqxQoEVi9BU4fvsML~ekt5WoMGrxos~e6BUvvTWd78nLD5LrUnJHYcc~FB8pK1C44Mp4PEqa-iJsvzYTPOshGdKHdSRYxg71v0BJH6QTOc-ui5TVWVrAbGuP9Pqc8mOAx0w87iXteIcdjneXXgj-mYW40w~cpuhof75vrtLUmBwRQvo8XRSAdE6trOoXBa7622HXtfPp1R29vxLbrleNQ__%22%7D", None)
 
 PING_IMG_URL = getenv(
-    "PING_IMG_URL",
+    "https://imagekit.io/tools/asset-public-link?detail=%7B%22name%22%3A%2299a178bee3952e9fe7e3313f4af5b550.jpg%22%2C%22type%22%3A%22image%2Fjpeg%22%2C%22signedurl_expire%22%3A%222028-03-24T06%3A27%3A26.129Z%22%2C%22signedUrl%22%3A%22https%3A%2F%2Fmedia-hosting.imagekit.io%2F%2F900bd7bd0d064698%2F99a178bee3952e9fe7e3313f4af5b550.jpg%3FExpires%3D1837492046%26Key-Pair-Id%3DK2ZIVPTIP2VGHC%26Signature%3DzS89l8F84mSfn7ElF4~j~gytIfyaRPQNN3WYs9lr7hfWGf8fNEJFoLwkzNvXTJoMQgfRnLey0kZoTky1-wbl0YpI1Ob~bGN93J0Ed86BDZhaCONJB4rpViHQR5P5MPrJ3btA6Sj2E8fCqfuYXArj5Lu7k-1dey6MZevNZ6ftBmWKPLLyqCi91JYrzb~xWDRihzyuTwPcwXPkaOrlEKbbnUblsBCJ99CZoVyYgt46BPHSib1KL1E930yDFxXCKAkLP~rM~1qNHnLblYSKtoqPeI9~La5Z4pk9hZj6C1jzhywWZXMuCbRu8MEjJWHHQ0tC3XIU4H9~FXKx4gbcLU74zg__%22%7D",
     "assets/Ping.jpeg",
 )
 
 PLAYLIST_IMG_URL = getenv(
-    "PLAYLIST_IMG_URL",
+    "https://imagekit.io/tools/asset-public-link?detail=%7B%22name%22%3A%2262e54b54e32a50dc07bc207f4ceee9d9.jpg%22%2C%22type%22%3A%22image%2Fjpeg%22%2C%22signedurl_expire%22%3A%222028-03-24T06%3A28%3A13.209Z%22%2C%22signedUrl%22%3A%22https%3A%2F%2Fmedia-hosting.imagekit.io%2F%2F514cb6868c454f37%2F62e54b54e32a50dc07bc207f4ceee9d9.jpg%3FExpires%3D1837492093%26Key-Pair-Id%3DK2ZIVPTIP2VGHC%26Signature%3DXyN-P2qs1bCwb4y70~VTpYNK8ODjCkIB8mqWb2NLHvmEHOCaRD5w8heL-Mp16yx4Wxp7xOcUygyrbju9c3GxSP39gmybs3A1XpPNAhTtP8HpBqf~faiGQHLj4wJQw8WvPCnP0bwMRPmhr5oFYpiyZ~VzAE9X0F2hwkDEqPK5K6Pfk9ORhel-vxjZ2hLfPY6pdLL~LLyRcbqWSAvZA7h9o8rIBGyVHLFxw5fBGcAWzqDLG5M8f5StBsX4kbWzl8-B5WUVCa9QTtx-MNmvL-R7-uU80JFomzR4Z5T2~dpx4qlYSaE7~SlWyh9NsfYxtKPwy~osyiKehUr9iRhHdpkbVg__%22%7D",
     "assets/Playlist.jpeg",
 )
 
 GLOBAL_IMG_URL = getenv(
-    "GLOBAL_IMG_URL",
+    "https://imagekit.io/tools/asset-public-link?detail=%7B%22name%22%3A%22d245a484765feb05098cbb825b41e0a8.jpg%22%2C%22type%22%3A%22image%2Fjpeg%22%2C%22signedurl_expire%22%3A%222028-03-24T06%3A28%3A53.922Z%22%2C%22signedUrl%22%3A%22https%3A%2F%2Fmedia-hosting.imagekit.io%2F%2Fbd7a4054124f4c15%2Fd245a484765feb05098cbb825b41e0a8.jpg%3FExpires%3D1837492134%26Key-Pair-Id%3DK2ZIVPTIP2VGHC%26Signature%3DUrS49qJR0FpPutrkaZX6v2RyMNFsVCGWcpYkD6MrcbvM3ay~kGQiKwvWH5UGyYngllebV8BDCDkNcwRU~GEh09smEQgvCLNbTFUfD0nNJ3pmDu8LwOneTp2Jm6ro~AzkFtMp6RZOgRqk~GAVF0RpsDeNz-hxNEJuynBhkEqWwvbk6jiPIPTwDXKwRI5XE3MlUNJePl~pRRdkvAjqPOLedIV-N-FZUP0~2znRFNHeqTXdT756ztX5zgm7Wj6FJyeKuh8IvId3pwU0vOgzshiY~IMO~Fat9FQq4M1O4F9jPDn2UWOBvQXjA2UWxonpNnbXk9HxlTdtvdvrUjwZYwQ1WQ__%22%7D",
     "assets/Global.jpeg",
 )
 
 STATS_IMG_URL = getenv(
-    "STATS_IMG_URL",
+    "https://imagekit.io/tools/asset-public-link?detail=%7B%22name%22%3A%22d56d6feb2b36091e2c12f9bd8b541972.jpg%22%2C%22type%22%3A%22image%2Fjpeg%22%2C%22signedurl_expire%22%3A%222028-03-24T06%3A29%3A26.154Z%22%2C%22signedUrl%22%3A%22https%3A%2F%2Fmedia-hosting.imagekit.io%2F%2F95a82013db3b4712%2Fd56d6feb2b36091e2c12f9bd8b541972.jpg%3FExpires%3D1837492166%26Key-Pair-Id%3DK2ZIVPTIP2VGHC%26Signature%3DSA0Olin4Bezi5XbNAzqFZfie~CRS~u2SucCGgr1qQi4zViqUS0l7hKdEYepv5WSVYjYY1fEZezfE9kMkF0ajTKtMrYZVc~SqFS8CMQ16CgEB0hpTI71~ptwXgppHOWhzAcSWt96sHfw7S335zMBbbROM8jeCDmg1G3Myru6Xo8Nmv~mRttJiX-ReoXB7bXzpQV5Z9YBbtD3SBmGFLmT~jssIqW0hNX4qX2P~4VKsGl1yYifLQYBnYVFwJUU6uE3oordYOxlXy9wF0OFUF1cVL~pkGOAN5RwUebSMwQfCLix-3g3PhCo7yIfydw0XO99uu1lecD8ZiAP6qA7d-9-ZUw__%22%7D",
     "assets/Stats.jpeg",
 )
 
@@ -201,32 +201,32 @@ TELEGRAM_VIDEO_URL = getenv(
 )
 
 STREAM_IMG_URL = getenv(
-    "STREAM_IMG_URL",
+    "https://imagekit.io/tools/asset-public-link?detail=%7B%22name%22%3A%22d245a484765feb05098cbb825b41e0a8.jpg%22%2C%22type%22%3A%22image%2Fjpeg%22%2C%22signedurl_expire%22%3A%222028-03-24T06%3A28%3A53.922Z%22%2C%22signedUrl%22%3A%22https%3A%2F%2Fmedia-hosting.imagekit.io%2F%2Fbd7a4054124f4c15%2Fd245a484765feb05098cbb825b41e0a8.jpg%3FExpires%3D1837492134%26Key-Pair-Id%3DK2ZIVPTIP2VGHC%26Signature%3DUrS49qJR0FpPutrkaZX6v2RyMNFsVCGWcpYkD6MrcbvM3ay~kGQiKwvWH5UGyYngllebV8BDCDkNcwRU~GEh09smEQgvCLNbTFUfD0nNJ3pmDu8LwOneTp2Jm6ro~AzkFtMp6RZOgRqk~GAVF0RpsDeNz-hxNEJuynBhkEqWwvbk6jiPIPTwDXKwRI5XE3MlUNJePl~pRRdkvAjqPOLedIV-N-FZUP0~2znRFNHeqTXdT756ztX5zgm7Wj6FJyeKuh8IvId3pwU0vOgzshiY~IMO~Fat9FQq4M1O4F9jPDn2UWOBvQXjA2UWxonpNnbXk9HxlTdtvdvrUjwZYwQ1WQ__%22%7D",
     "assets/Stream.jpeg",
 )
 
 SOUNCLOUD_IMG_URL = getenv(
-    "SOUNCLOUD_IMG_URL",
+    "https://imagekit.io/tools/asset-public-link?detail=%7B%22name%22%3A%22d56d6feb2b36091e2c12f9bd8b541972.jpg%22%2C%22type%22%3A%22image%2Fjpeg%22%2C%22signedurl_expire%22%3A%222028-03-24T06%3A29%3A26.154Z%22%2C%22signedUrl%22%3A%22https%3A%2F%2Fmedia-hosting.imagekit.io%2F%2F95a82013db3b4712%2Fd56d6feb2b36091e2c12f9bd8b541972.jpg%3FExpires%3D1837492166%26Key-Pair-Id%3DK2ZIVPTIP2VGHC%26Signature%3DSA0Olin4Bezi5XbNAzqFZfie~CRS~u2SucCGgr1qQi4zViqUS0l7hKdEYepv5WSVYjYY1fEZezfE9kMkF0ajTKtMrYZVc~SqFS8CMQ16CgEB0hpTI71~ptwXgppHOWhzAcSWt96sHfw7S335zMBbbROM8jeCDmg1G3Myru6Xo8Nmv~mRttJiX-ReoXB7bXzpQV5Z9YBbtD3SBmGFLmT~jssIqW0hNX4qX2P~4VKsGl1yYifLQYBnYVFwJUU6uE3oordYOxlXy9wF0OFUF1cVL~pkGOAN5RwUebSMwQfCLix-3g3PhCo7yIfydw0XO99uu1lecD8ZiAP6qA7d-9-ZUw__%22%7D",
     "assets/Soundcloud.jpeg",
 )
 
 YOUTUBE_IMG_URL = getenv(
-    "YOUTUBE_IMG_URL",
+    "https://imagekit.io/tools/asset-public-link?detail=%7B%22name%22%3A%223dc70fe88c2cb49d62b91a8cf4b4fae6.jpg%22%2C%22type%22%3A%22image%2Fjpeg%22%2C%22signedurl_expire%22%3A%222028-03-24T06%3A30%3A45.848Z%22%2C%22signedUrl%22%3A%22https%3A%2F%2Fmedia-hosting.imagekit.io%2F%2F6eb612451b664e75%2F3dc70fe88c2cb49d62b91a8cf4b4fae6.jpg%3FExpires%3D1837492246%26Key-Pair-Id%3DK2ZIVPTIP2VGHC%26Signature%3Djvtd~cbgiChpMRlaTrEvvFXJKnWDK7E2AAfVtzZnHfCCg8xI4BLEnOBuvAn791lkplgD9MrsMh2YAP07TLuJdrQql-2EgSLawDPJbqLbQCOAIHam8oCjDF4km-pdC9jzcuhVx3i7fA3thg3yC2GWpreAG3alNfpeGfubM14Ih94mQY4jTm9jhzUl3SnHy8uMvR-PrXlCFp4EiOV4FlTFCK-JTeYPRdwzDG8~TUsmDpfH4GXBaLBoIoLE2xBrXbcWcucPCJLE0r8s2kO1DakFuAUWIf0kH-7tNv4c~HYZnJHrxqePf4ShFLQJOqIcr61-IuQ7vi4htRN28Z~WpIag~A__%22%7D",
     "assets/Youtube.jpeg",
 )
 
 SPOTIFY_ARTIST_IMG_URL = getenv(
-    "SPOTIFY_ARTIST_IMG_URL",
+    "https://imagekit.io/tools/asset-public-link?detail=%7B%22name%22%3A%2208929cd487b958e3f6975a7c23f143f4.jpg%22%2C%22type%22%3A%22image%2Fjpeg%22%2C%22signedurl_expire%22%3A%222028-03-24T06%3A31%3A35.445Z%22%2C%22signedUrl%22%3A%22https%3A%2F%2Fmedia-hosting.imagekit.io%2F%2F9da280c5ffbd4e4e%2F08929cd487b958e3f6975a7c23f143f4.jpg%3FExpires%3D1837492295%26Key-Pair-Id%3DK2ZIVPTIP2VGHC%26Signature%3DbSSQNL3TI0zbDddiDqhgIHooSyVilBbz4~Pu94Tv77Y639ye3dj~WXlry4M4Cg~7LW2HJriYmiIl6BLulcU~rNlAoHv~vGEapbF0nJr0CkHkxBInrgYYdGg4-rZ6bE2jWwXDuef~0cPTyANiwxbEj5jbQSYi~qoZbi~d8vaQttO~UloSt7-yAA7OlQQUrOq58tfHU03GbZdAh6D0ZXqXQrzpDDql1Y0oXK1lNPwyRIhuh5bm7qLpLVb5bjc6Fa8jcHeG9ALftro~CGAxcF2bpexJFV1HFsbcSg7u2GfNPq6fbzGw-LbptaaVevu9guo0Y5zOv5QtRLIN0URfmGPWiA__%22%7D",
     "assets/SpotifyArtist.jpeg",
 )
 
 SPOTIFY_ALBUM_IMG_URL = getenv(
-    "SPOTIFY_ALBUM_IMG_URL",
+    "https://imagekit.io/tools/asset-public-link?detail=%7B%22name%22%3A%223a136589b6087156cf280a65cc88089b.jpg%22%2C%22type%22%3A%22image%2Fjpeg%22%2C%22signedurl_expire%22%3A%222028-03-24T06%3A31%3A35.443Z%22%2C%22signedUrl%22%3A%22https%3A%2F%2Fmedia-hosting.imagekit.io%2F%2F156801a022a4442e%2F3a136589b6087156cf280a65cc88089b.jpg%3FExpires%3D1837492295%26Key-Pair-Id%3DK2ZIVPTIP2VGHC%26Signature%3DW-LYP9op1~U0S~qUultGqSuezPKhKP5A~GJ9JiocIVDh0Xok20wBwUhWKZAhrZQrq~HdwQDxG7cHgqmOyi2F7dVmONgJVfIWlot9v-eKT8Rk~i16VFBjsg1oOBTSICAHkpmZCrrz51hI-jLnYNFhpih01nULLU8M7OQCp37~7U2R8BBCgJzJ1g8h29fK3gWLsq10HvfxlIzV74u4UyVG7ckEN4HB24nkBjB7jTVK8BJc0TGryUN3rtzEtOA1VVYdjReoYPPvn7ylHomuYc~iuuIxem2pI5hZUoKjaJ1JUaL9KGo-jB02u99V72K~~D-j1Q3z0ZbCwt3SlE2MejUMsA__%22%7D",
     "assets/SpotifyAlbum.jpeg",
 )
 
 SPOTIFY_PLAYLIST_IMG_URL = getenv(
-    "SPOTIFY_PLAYLIST_IMG_URL",
+    "https://imagekit.io/tools/asset-public-link?detail=%7B%22name%22%3A%229f05abb4dde3d1d692ec69a8487e8fcb.jpg%22%2C%22type%22%3A%22image%2Fjpeg%22%2C%22signedurl_expire%22%3A%222028-03-24T06%3A31%3A35.438Z%22%2C%22signedUrl%22%3A%22https%3A%2F%2Fmedia-hosting.imagekit.io%2F%2Fd05e9ef333344246%2F9f05abb4dde3d1d692ec69a8487e8fcb.jpg%3FExpires%3D1837492295%26Key-Pair-Id%3DK2ZIVPTIP2VGHC%26Signature%3DAv-RD-wDPQ80pODe49VgVUgvHyBFd9-k-qce9M2zVSdgxcEqTHKETz6yJQ84US6yfoqPIwWzAaq855~rh6keCkYpWcNMFlw0YAQnCjA1L1vh674wgYYLaZbh-2HUgoBON2suaZ2eITDQIGFKpjJTXiFbMKTu4byCkMTinAv0NMqaT3U8JX4m8uRZ2IEdKC-k4i6iMeTV61WgPbKD9TeeuMSe9bX9XCWw8EjUnbsQ4MqHFjEv~QJ6jZ6rXlrjQbPwVmm10fVLA6iZ3OFpvSFw1ci2JxWs-TCYKWdhVFa-oaUNnWfPzmNd-ugvK99y0WgqDM2pKhW-3BM4ptqnW3HV0Q__%22%7D",
     "assets/SpotifyPlaylist.jpeg",
 )
 
